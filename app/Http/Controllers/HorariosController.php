6d10fb08afc8f36df46d6140f651a7c538a32371
George-Trindade <georgetrindade.dev@gmail.com>
2023-03-01 20:33:01 -0300

    Limitação de requesições por horário

--- app/Http/Controllers/HorariosController.php
+++ app/Http/Controllers/HorariosController.php
@@ -10,47 +10,57 @@ use Illuminate\Support\Facades\DB;
 
 class HorariosController extends Controller
 {
-    public function horario(Request $request, $datadigitada){
+    public function horario(Request $request, $datadigitada)
+    {
 
         //$agendamentos = Agendamento::query();
         //$datadigitada=$request->query->get('data_capturada');
-        $status='solicitado';
 
-        $agendamentos = DB::table('agendamentos')->where('date','LIKE', $datadigitada)->get();
-        $ContAgendamentos =$agendamentos->count();
+        $agendamentos = DB::table('agendamentos')->where('date', 'LIKE', $datadigitada)->get();
+        $ContAgendamentos = $agendamentos->count();
         $horarios = DB::table('horarios')->get();
-        $cont=0;
-        $horariosDisponiveis=$horarios;
-        if($ContAgendamentos>0){
-            foreach ($agendamentos as $agendamento){
-                foreach ($horarios as $key => $horario){
-                    if($agendamento->status=="agendado" && $agendamento->horario == $horario->hora){
+        $horariosDisponiveis = $horarios;
+        $cont = 0;
+
+        if ($ContAgendamentos > 0) {
+            foreach ($agendamentos as $agendamento) {
+                foreach ($horarios as $key => $horario) {
+                    if ($agendamento->status == "agendado" && $agendamento->horario == $horario->hora) {
                         unset($horariosDisponiveis[$key]);
+                    }
+                }
+            }
+            foreach ($agendamentos as $agendamento) {
+                foreach ($horariosDisponiveis as $key => $horario) {
+                    if ($agendamento->status == "solicitado" && $agendamento->horario == $horario->hora) {
+                        $cont++;
+                        if ($cont > 3) {
+                            unset($horariosDisponiveis[$key]);
+                        }
                     }
                 }
             }
 
             return response()->json($horariosDisponiveis);
-
-        }else{
+        } else {
             return response()->json($horarios);
         }
-
     }
-    public function verificaHorario(Request $request, $datadigitada, $horario){
-        $status="agendado";
-        $user=Auth::id();
-        $agendamentoUser = DB::table('agendamentos')->where('date','LIKE', $datadigitada)->where('horario','LIKE', $horario)->where('status','LIKE', 'solicitado')->where('user_id','LIKE',$user)->get()->count();
+    public function verificaHorario(Request $request, $datadigitada, $horario)
+    {
+        $status = "agendado";
+        $user = Auth::id();
+        $agendamentoUser = DB::table('agendamentos')->where('date', 'LIKE', $datadigitada)->where('horario', 'LIKE', $horario)->where('status', 'LIKE', 'solicitado')->where('user_id', 'LIKE', $user)->get()->count();
         //dd($agendamentoUser);
-        $agendamentos = DB::table('agendamentos')->where('date','LIKE', $datadigitada)->where('horario','LIKE', $horario)->where('status','LIKE', $status)->get();
-        $ContAgendamentos =$agendamentos->count();
-        if($agendamentoUser>1){
-            return response()->json(['success'=>true]);
-        }else{
-            if($ContAgendamentos>0){
-                return response()->json(['success'=>true]);
-            }else{
-                return response()->json(['success'=>false]);
+        $agendamentos = DB::table('agendamentos')->where('date', 'LIKE', $datadigitada)->where('horario', 'LIKE', $horario)->where('status', 'LIKE', $status)->get();
+        $ContAgendamentos = $agendamentos->count();
+        if ($agendamentoUser > 1) {
+            return response()->json(['success' => true]);
+        } else {
+            if ($ContAgendamentos > 0) {
+                return response()->json(['success' => true]);
+            } else {
+                return response()->json(['success' => false]);
             }
         }
     }
