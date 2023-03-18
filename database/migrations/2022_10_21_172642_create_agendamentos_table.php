d60d78aa148504d13133bada1ba5eac1fa5ba4b7
George-Trindade <georgetrindade.dev@gmail.com>
2023-03-17 21:28:23 -0300

    migration

--- database/migrations/2022_10_21_172642_create_agendamentos_table.php
+++ database/migrations/2022_10_21_172642_create_agendamentos_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\Models\Veiculo;
+
 return new class extends Migration
 {
     /**
@@ -18,7 +19,8 @@ return new class extends Migration
             $table->string('servico');
             $table->string('date');
             $table->string('horario');
-            $table->foreign('veiculo_id')->references('id')->on('veiculo');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('veiculo_id')->constrained();
             $table->string('status');
             $table->softDeletes();
             $table->timestamps();
