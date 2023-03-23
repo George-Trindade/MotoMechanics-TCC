a8f95330f349669ac70a77d63b8e3c2d4de1168b
George-Trindade <georgetrindade.dev@gmail.com>
2023-03-22 22:23:48 -0300

    erro sintaxe href

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -90,7 +90,7 @@
     {{-- Configured Scripts --}}
     @include('adminlte::plugins', ['type' => 'js'])
 
-    <script src="http://motomechanics.online/public/vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
+    <script src="http://motomechanics.online/public/vendor/adminlte/dist/js/adminlte.min.js"></script>
     @else
     <script src="{{ mix(config('adminlte.laravel_mix_js_path', 'js/app.js')) }}"></script>
     @endif
