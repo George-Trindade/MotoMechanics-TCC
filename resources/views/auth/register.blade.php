66ab1a38abe1d32cdc2ef56fe51f73ea6a8d0276
George-Trindade <georgetrindade.dev@gmail.com>
2023-04-02 16:32:08 -0300

    Teste de include

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Registrar-MotoMechanics</title>
     <link rel="stylesheet" type="text/css" href="http://motomechanics.online/public/assets/css/fomantic/dist/semantic.min.css">
     <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
     <script src="http://motomechanics.online/public/assets/css/fomantic/dist/semantic.js"></script>
---
+++ resources/views/teste.blade.php
@@ -0,0 +1,6 @@
+@include('layouts.header')
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque accusamus deserunt iure, ducimus quisquam assumenda omnis nam minus illum quo magni quas repellendus enim, incidunt quos esse ad nobis corporis!</p>
+
+
+@include('layouts.footer')
