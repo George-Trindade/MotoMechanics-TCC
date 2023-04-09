3803a9b9c90a9255eacdcfe8de8f25f8c5b160a1
George-Trindade <georgetrindade.dev@gmail.com>
2023-04-09 19:22:38 -0300

    color menu

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -17,10 +17,10 @@
 
 <!-- Sidebar Menu -->
 <div class="ui vertical inverted sidebar menu">
-    <a class="active item">Home</a>
-    <a class="item">Work</a>
-    <a class="item">Company</a>
-    <a class="item">Careers</a>
+    <a class="active item">Agendamentos</a>
+    <a class="item">Orçamentos</a>
+    <a class="item">Meus veículos</a>
+    <!-- <a class="item">Careers</a>
     <a class="item">Login</a>
-    <a class="item">Signup</a>
+    <a class="item">Signup</a> -->
 </div>
