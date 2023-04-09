ae65c50d08dc753778ac5510b36e129b81db0d41
George-Trindade <georgetrindade.dev@gmail.com>
2023-04-09 19:11:15 -0300

    url imports

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -9,29 +9,29 @@
 
     <!-- Site Properties -->
     <title>Base</title>
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/components/footer.css">
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/fomantic/dist/components/reset.css">
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/fomantic/dist/components/site.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/components/footer.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/fomantic/dist/components/reset.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/fomantic/dist/components/site.css">
 
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/fomantic/dist/components/container.css">
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/fomantic/dist/components/grid.css">
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/fomantic/dist/components/header.css">
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/fomantic/dist/components/image.css">
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/fomantic/dist/components/menu.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/fomantic/dist/components/container.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/fomantic/dist/components/grid.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/fomantic/dist/components/header.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/fomantic/dist/components/image.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/fomantic/dist/components/menu.css">
 
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/fomantic/dist/components/divider.css">
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/fomantic/dist/components/dropdown.css">
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/fomantic/dist/components/segment.css">
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/fomantic/dist/components/button.css">
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/fomantic/dist/components/list.css">
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/fomantic/dist/components/icon.css">
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/fomantic/dist/components/sidebar.css">
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/fomantic/dist/components/transition.css">
-    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/assets/css/components/base_pag.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/fomantic/dist/components/divider.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/fomantic/dist/components/dropdown.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/fomantic/dist/components/segment.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/fomantic/dist/components/button.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/fomantic/dist/components/list.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/fomantic/dist/components/icon.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/fomantic/dist/components/sidebar.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/fomantic/dist/components/transition.css">
+    <link rel="stylesheet" type="text/css" href="https://motomechanics.online/public/assets/css/components/base_pag.css">
     <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
-    <script src="https://motomechanics.online/assets/css/fomantic/dist/components/visibility.js"></script>
-    <script src="https://motomechanics.online/assets/css/fomantic/dist/components/sidebar.js"></script>
-    <script src="https://motomechanics.online/assets/css/fomantic/dist/components/transition.js"></script>
+    <script src="https://motomechanics.online/public/assets/css/fomantic/dist/components/visibility.js"></script>
+    <script src="https://motomechanics.online/public/assets/css/fomantic/dist/components/sidebar.js"></script>
+    <script src="https://motomechanics.online/public/assets/css/fomantic/dist/components/transition.js"></script>
 
 </head>
 
